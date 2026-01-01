55f4f9212465161d18011dd0cc1fca107c4bbe29
Yuritzi Calixto <ramirezcalixtodiana@gmail.com>
2026-01-01 17:58:50 -0600

    Change

---
+++ variables.js
@@ -0,0 +1,13 @@
+// let msg = prompt("Ingresa un numero: ");
+// let a = 20;
+// let b = 5;
+let suma = a + b;
+
+
+for ( let numero = 1; numero < 20; numero++){
+     console.log(numero);
+}
+
+
+
+// console.log(numero);
