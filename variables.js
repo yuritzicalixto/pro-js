2dd80b40a7b9fde06a9f2f97a415519e84b5e7ef
Yuritzi Calixto <ramirezcalixtodiana@gmail.com>
2026-01-02 18:09:50 -0600

    For

--- variables.js
+++ variables.js
@@ -1,13 +1,21 @@
 // let msg = prompt("Ingresa un numero: ");
 // let a = 20;
 // let b = 5;
-let suma = a + b;
+// let suma = a + b;
 
 
-for ( let numero = 1; numero < 20; numero++){
-     console.log(numero);
-}
+// for ( let numero = 1; numero < 20; numero++){
+//      console.log(numero);
+// }
 
 
 
 // console.log(numero);
+
+for (let n = 1; n <= 10; n++) {
+     if (n%2 ===0) {
+          console.log("El numero " +n+ " es par");
+     } if(n%2 ===1){
+          console.log("El numero " +n+ " es impar");
+     }
+}
